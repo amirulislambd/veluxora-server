e6cdb55accba149ba13c0acbe8997c8fe3dd24cb
amirulislambd <amirulislambd313@gmail.com>
2026-05-18 06:58:15 +0600

    conneted to database

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_DB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("bismaillahir rahmanir rahim");
+});
+
+app.listen(port, () => {
+  console.log(`Running app listening on port ${port}`);
+});
