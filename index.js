2501d50b31fbec82b3a558792c7c625208b0c9d8
amirulislambd <amirulislambd313@gmail.com>
2026-05-20 14:22:53 +0600

    feat: add endpoint to fetch single car by ID

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/cars/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await carCollection.findOne(query);
+      res.send(result);
+    });
+
+
   } finally {
     // await client.close();
   }
